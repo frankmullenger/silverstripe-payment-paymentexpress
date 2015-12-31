f650ef5462401e9d9073e94c57e0e85919f9552e
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:43:29 +1300

    Converted to PSR-2

--- code/PaymentExpressGateway.php
+++ code/PaymentExpressGateway.php
@@ -1,51 +1,58 @@
 <?php
 
-class PaymentExpressGateway_PxPay extends PaymentGateway_GatewayHosted {
-	
-	protected $pxPayUrl;
-	protected $pxPayUserID;
-	protected $pxPayKey;
+class PaymentExpressGateway_PxPay extends PaymentGateway_GatewayHosted
+{
+    
+    protected $pxPayUrl;
+    protected $pxPayUserID;
+    protected $pxPayKey;
 
-  protected $supportedCurrencies = array(
+    protected $supportedCurrencies = array(
     'NZD' => 'New Zealand Dollar',
     'USD' => 'United States Dollar',
     'GBP' => 'Great British Pound'
   );
 
-  public function getSupportedCurrencies() {
-
-    $config = $this->getConfig();
-    if (isset($config['supported_currencies'])) {
-      $this->supportedCurrencies = $config['supported_currencies'];
+    public function getSupportedCurrencies()
+    {
+        $config = $this->getConfig();
+        if (isset($config['supported_currencies'])) {
+            $this->supportedCurrencies = $config['supported_currencies'];
+        }
+        return $this->supportedCurrencies;
     }
-    return $this->supportedCurrencies;
-  }
   
-  protected function onBeforeMakeRequest() {
-  	$config = $this->getConfig();
+    protected function onBeforeMakeRequest()
+    {
+        $config = $this->getConfig();
 
-    $this->pxPayUrl    = Config::inst()->get('PaymentExpressGateway_PxPay', 'url');
-    $this->pxPayUserID = $config['authentication']['user_id'];
-    $this->pxPayKey    = $config['authentication']['key'];
-  }
+        $this->pxPayUrl    = Config::inst()->get('PaymentExpressGateway_PxPay', 'url');
+        $this->pxPayUserID = $config['authentication']['user_id'];
+        $this->pxPayKey    = $config['authentication']['key'];
+    }
   
-  public function process($data) {
+    public function process($data)
+    {
 
     //Construct the request
     $request = new PxPayRequest();
-    $request->setAmountInput($data['Amount']);
-    $request->setCurrencyInput($data['Currency']);
+        $request->setAmountInput($data['Amount']);
+        $request->setCurrencyInput($data['Currency']);
 
     //Set PxPay properties
-    if (isset($data['Reference'])) $request->setMerchantReference($data['Reference']);
-		if (isset($data['EmailAddress'])) $request->setEmailAddress($data['EmailAddress']);
+    if (isset($data['Reference'])) {
+        $request->setMerchantReference($data['Reference']);
+    }
+        if (isset($data['EmailAddress'])) {
+            $request->setEmailAddress($data['EmailAddress']);
+        }
 
-    $request->setUrlFail($this->cancelURL);
-    $request->setUrlSuccess($this->returnURL);
+        $request->setUrlFail($this->cancelURL);
+        $request->setUrlSuccess($this->returnURL);
 
     //Generate a unique identifier for the transaction
-    $request->setTxnId(uniqid('ID')); 
-    $request->setTxnType('Purchase');
+    $request->setTxnId(uniqid('ID'));
+        $request->setTxnType('Purchase');
     
     //Get encrypted URL from DPS to redirect the user to
     $request_string = $this->makeProcessRequest($request, $data);
@@ -55,45 +62,43 @@ class PaymentExpressGateway_PxPay extends PaymentGateway_GatewayHosted {
 
     //Parse output XML
     $url = $response->get_element_text('URI');
-    $valid = $response->get_attribute('valid');
+        $valid = $response->get_attribute('valid');
 
     //If this is a fail or incomplete (cannot reach gateway) then mark payment accordingly and redirect to payment 
     if ($valid && is_numeric($valid) && $valid == 1) {
-    	//Redirect to payment page
-    	Controller::curr()->redirect($url);
+        //Redirect to payment page
+        Controller::curr()->redirect($url);
+    } elseif (is_numeric($valid) && $valid == 0) {
+        return new PaymentGateway_Failure();
+    } else {
+        return new PaymentGateway_Incomplete();
     }
-    else if (is_numeric($valid) && $valid == 0) {
-    	return new PaymentGateway_Failure();
     }
-    else {
-    	return new PaymentGateway_Incomplete();
-    }
-  }
   
-  public function makeProcessRequest($request, $data) {
-		
-		$this->onBeforeMakeRequest();
-  	$pxpay = new PxPay_Curl($this->pxPayUrl, $this->pxPayUserID, $this->pxPayKey);
-  	return $pxpay->makeRequest($request);
-	}
+    public function makeProcessRequest($request, $data)
+    {
+        $this->onBeforeMakeRequest();
+        $pxpay = new PxPay_Curl($this->pxPayUrl, $this->pxPayUserID, $this->pxPayKey);
+        return $pxpay->makeRequest($request);
+    }
   
   /**
    * Check that the payment was successful using "Process Response" API (http://www.paymentexpress.com/Technical_Resources/Ecommerce_Hosted/PxPay.aspx).
    * 
    * @param SS_HTTPRequest $request Request from the gateway - transaction response
    * @return PaymentGateway_Result
-   */ 
-	public function check($request) {
+   */
+    public function check($request)
+    {
+        $data = $request->getVars();
 
-		$data = $request->getVars();
-
-		$url = $request->getVar('url');
-		$result = $request->getVar('result');
-		$userID = $request->getVar('userid');
-		
-		//Construct the request to check the payment status
+        $url = $request->getVar('url');
+        $result = $request->getVar('result');
+        $userID = $request->getVar('userid');
+        
+        //Construct the request to check the payment status
     $request = new PxPayLookupRequest();
-    $request->setResponse($result);
+        $request->setResponse($result);
 
     //Get encrypted URL from DPS to redirect the user to
     $request_string = $this->makeCheckRequest($request, $data);
@@ -104,73 +109,72 @@ class PaymentExpressGateway_PxPay extends PaymentGateway_GatewayHosted {
     //Parse output XML
     $success = $response->get_element_text('Success');
 
-    if ($success && is_numeric($success) && $success > 0) {
-    	return new PaymentGateway_Success();
+        if ($success && is_numeric($success) && $success > 0) {
+            return new PaymentGateway_Success();
+        } elseif (is_numeric($success) && $success == 0) {
+            return new PaymentGateway_Failure();
+        } else {
+            return new PaymentGateway_Incomplete();
+        }
     }
-    else if (is_numeric($success) && $success == 0) {
-    	return new PaymentGateway_Failure();
+    
+    public function makeCheckRequest($request, $data)
+    {
+        $this->onBeforeMakeRequest();
+        $pxpay = new PxPay_Curl($this->pxPayUrl, $this->pxPayUserID, $this->pxPayKey);
+        return $pxpay->makeRequest($request);
     }
-    else {
-    	return new PaymentGateway_Incomplete();
-    }
-	}
-	
-	public function makeCheckRequest($request, $data) {
-		
-		$this->onBeforeMakeRequest();
-		$pxpay = new PxPay_Curl($this->pxPayUrl, $this->pxPayUserID, $this->pxPayKey);
-  	return $pxpay->makeRequest($request);
-	}
 }
 
-class PaymentExpressGateway_PxPay_Mock extends PaymentExpressGateway_PxPay {
+class PaymentExpressGateway_PxPay_Mock extends PaymentExpressGateway_PxPay
+{
 
-  public function makeProcessRequest($request, $data) {
-
-  	$this->onBeforeMakeRequest();
+    public function makeProcessRequest($request, $data)
+    {
+        $this->onBeforeMakeRequest();
 
     //Mock request string
     $mock = isset($data['mock']) ? $data['mock'] : false;
-    if ($mock) {
-    	switch($mock) {
-	    	case 'incomplete':
-	    		$request_string = false;
-	    		break;
-	    	case 'failure':
-	    		$request_string = "
+        if ($mock) {
+            switch ($mock) {
+            case 'incomplete':
+                $request_string = false;
+                break;
+            case 'failure':
+                $request_string = "
 		    	<Request valid=\"0\">
 						<URI></URI>
 					</Request>";
-	    		break;
-	    	case 'success':
-	    	default:
-	    		$request_string = "
+                break;
+            case 'success':
+            default:
+                $request_string = "
 		    	<Request valid=\"1\">
 						<URI>{$this->pxPayUrl}?userid={$this->pxPayUserID}&amp;request=v52CRsqBR5-mock</URI>
 					</Request>";
-	    		break;	
-	    }
-    }
-    else {
-    	throw new Exception('Mock string not passed');
-    }
+                break;
+        }
+        } else {
+            throw new Exception('Mock string not passed');
+        }
     
-    return $request_string;
-	}
+        return $request_string;
+    }
 
-	public function makeCheckRequest($request, $data) {
+    public function makeCheckRequest($request, $data)
+    {
 
     //Mock request string
     $mock = isset($data['mock']) ? $data['mock'] : false;
-    if ($mock) {
-    	switch($mock) {
+        if ($mock) {
+            switch ($mock) {
 
-	    	//Gateway could not be reached, curl_exec returns false
-	    	case 'incomplete':
-	    		$request_string = false;
-	    		break;
-	    	case 'failure':
-	    		$request_string = '
+            //Gateway could not be reached, curl_exec returns false
+            case 'incomplete':
+                $request_string = false;
+                break;
+            case 'failure':
+                $request_string = '
 			    <Response valid="1">
 			    	<Success>0</Success>
 			    	<TxnType>Purchase</TxnType>
@@ -200,10 +204,10 @@ class PaymentExpressGateway_PxPay_Mock extends PaymentExpressGateway_PxPay {
 			    	<Cvc2ResultCode>NotUsed</Cvc2ResultCode>
 			    	<ReCo>00</ReCo>
 			    </Response>';
-	    		break;
-	    	case 'success':
-	    	default:
-	    		$request_string = '
+                break;
+            case 'success':
+            default:
+                $request_string = '
 			    <Response valid="1">
 			    	<Success>1</Success>
 			    	<TxnType>Purchase</TxnType>
@@ -233,16 +237,12 @@ class PaymentExpressGateway_PxPay_Mock extends PaymentExpressGateway_PxPay {
 			    	<Cvc2ResultCode>NotUsed</Cvc2ResultCode>
 			    	<ReCo>00</ReCo>
 			    </Response>';
-	    		break;
-	    }
-    }
-    else {
-    	throw new Exception('Mock string not passed');
-    }
+                break;
+        }
+        } else {
+            throw new Exception('Mock string not passed');
+        }
     
-    return $request_string;
-	}
-
+        return $request_string;
+    }
 }
-
-
